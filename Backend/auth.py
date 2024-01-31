01ca9dfae374f5f992f13069b680f8f19f5ed3d8
Dinobi <architectstan01@gmail.com>
2024-01-31 13:40:18 +0100

    My workspace

---
+++ Backend/auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Containes all the Authentication processes from the Phone verification to site access"""
+
+class Auth:
+    def __init__(self, db):
+        pass
+    """"""
